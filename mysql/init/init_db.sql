474783a769b2c58e0bf9a138a49f5e9f4d001935
JustARand0m <korbinian.federholzner@t-online.de>
2019-07-19 16:58:23 +0200

    changed somehting in database schema

--- mysql/init/init_db.sql
+++ mysql/init/init_db.sql
@@ -10,7 +10,7 @@ CREATE TABLE todos (
     id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     isDone INT,
     todoMsg VARCHAR(255),
-    importance INT NOT NULL CHECK(importance BETWEEN 0 AND 2),
+    importance INT NOT NULL CHECK(importance BETWEEN 0 AND 3),
     endDate DATE,
     details VARCHAR(255),
     imgUrl VARCHAR(255)
